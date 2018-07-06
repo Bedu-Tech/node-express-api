a4c499aafa83c3541bc9e6f200292433803fd7e6
Javier Diaz <coderdiaz@gmail.com>
2018-07-05 20:12:19 -0500

    Added nodemon and dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
 
+// MongoUrl from environment (file .env)
+const MONGO_URL = process.env.MONGO_URL || '';
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/bedudb'),
+    MongooseModule.forRoot(MONGO_URL),
     ProductModule,
   ],
 })
