7646a6f7e5c492531fb77b45d2f9e3f2a6aab4a4
Javier Diaz <coderdiaz@gmail.com>
2018-07-10 20:17:22 -0500

    Added port var to environment

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 
+const PORT = process.env.PORT || 3000;
+
 const bootstrap = async () => {
   const app = await NestFactory.create(ApplicationModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 };
 
 bootstrap();
